70a830b231209bf9e6d9161907ccd6dd5b438248
Thanh Van Vu <thanh.vu@hitachivantara.com>
2021-07-11 01:00:14 +0700

    Integrate stripe payment gateway in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Elements } from '@stripe/react-stripe-js';
 
-function App() {
+import { cart, successMessage } from 'utils';
+import CheckoutForm from 'components/checkout-form';
+import stripePromise from 'config/stripe.config';
+
+const App = () => {
+  const [paymentCompleted, setPaymentCompleted] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row s-box">
+        {paymentCompleted ? successMessage() : <React.Fragment>
+          <div className="col-md-5 order-md-2 mb-4">
+            {cart()}
+          </div>
+          <div className="col-md-7 order-md-1">
+            <Elements stripe={stripePromise}>
+              <CheckoutForm amount={2000} setPaymentCompleted={setPaymentCompleted} />
+            </Elements>
+          </div>
+        </React.Fragment>}
+      </div>
+
     </div>
   );
-}
+};
 
 export default App;
